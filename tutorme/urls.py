11ef227444a9e7ac48eda5769108f0b5c47b1a93
Saarthak Gupta <79454214+saarthak2002@users.noreply.github.com>
2023-02-18 16:04:58 -0500

    fixed template URI in urls.py

--- tutorme/urls.py
+++ tutorme/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="index.html")),
+    path('', TemplateView.as_view(template_name="tutorme/index.html")),
     path('accounts/', include('allauth.urls')),
     path('logout', LogoutView.as_view()),
 ]
