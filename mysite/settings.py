c8de111bcd0d075f81c8fa63e749470f7888a44a
Saarthak Gupta <79454214+saarthak2002@users.noreply.github.com>
2023-02-18 15:39:31 -0500

    added site_id and redirect urls

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,10 +131,28 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+# Set up for Google Login
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend'
 ]
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
+
+SITE_ID = 2
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Activate Django-Heroku.
 # Use this code to avoid the psycopg2 / django-heroku error!  
