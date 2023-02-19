e00f033b8c074dd542a86de866cab655c3fef90a
Saarthak Gupta <79454214+saarthak2002@users.noreply.github.com>
2023-02-19 16:38:17 -0500

    added basic styles to page

--- tutorme/urls.py
+++ tutorme/urls.py
@@ -6,6 +6,8 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('', TemplateView.as_view(template_name="tutorme/index.html")),
     path('accounts/google/login/callback/tutorme/', TemplateView.as_view(template_name="tutorme/index.html")),
+    path('accounts/social/signup/tutorme/', TemplateView.as_view(template_name="tutorme/index.html")),
+    path('accounts/logout/tutorme/', TemplateView.as_view(template_name="tutorme/index.html")),
     path('accounts/', include('allauth.urls')),
     path('logout', LogoutView.as_view()),
 ]
