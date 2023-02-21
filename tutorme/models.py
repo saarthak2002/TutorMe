f0d355f0debeec6548073f9806ebd3360048a9dd
Saarthak Gupta <79454214+saarthak2002@users.noreply.github.com>
2023-02-21 18:16:55 -0500

    extended user model to implement 2 types of users

--- tutorme/models.py
+++ tutorme/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class AppUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    USER_TYPE_CHOICES = (
+      (1, 'student'),
+      (2, 'tutor'),
+    )
+    user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES)
+    def __str__(self):
+        return self.user.username
+
