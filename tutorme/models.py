d86aec427c7e3b54e1f5d4c0738053282e8c3f3b
Saarthak Gupta <79454214+saarthak2002@users.noreply.github.com>
2023-02-23 12:51:28 -0500

    added signal handler for default foreign key

--- tutorme/models.py
+++ tutorme/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 # Create your models here.
 class AppUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -8,7 +11,11 @@ class AppUser(models.Model):
       (1, 'student'),
       (2, 'tutor'),
     )
-    user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES)
+    user_type = models.PositiveSmallIntegerField(default=1, choices=USER_TYPE_CHOICES)
     def __str__(self):
         return self.user.username
 
+@receiver(post_save, sender=User)
+def app_user_create(sender, instance=None, created=False, **kwargs):
+    if created:
+        AppUser.objects.create(user=instance,)
