18d84f23be279891f3d8b2c8e336b8047def1aee
Saarthak Gupta <79454214+saarthak2002@users.noreply.github.com>
2023-03-05 13:55:25 -0500

    registered model Request

--- tutorme/admin.py
+++ tutorme/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import AppUser, Tutor
+from .models import AppUser, Tutor, Request
 # Register your models here.
 
 admin.site.register(AppUser)
 admin.site.register(Tutor)
+admin.site.register(Request)
