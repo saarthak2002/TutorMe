d0cffc4eced89722f742155d0f043086bbfa1015
Alex Catahan <rac4sq@virginia.edu>
2023-04-01 12:48:02 -0400

    added time to student's requests

--- tutorme/models.py
+++ tutorme/models.py
@@ -38,7 +38,9 @@ class Request(models.Model):
       (3, 'declined')
     )
     status = models.PositiveSmallIntegerField(choices=REQUEST_STATUS_CHOICES, default=1)
-
+    date_requested = models.DateField(default=datetime.now)
+    start_time_requested = models.TimeField(default=datetime.now)
+    end_time_requested = models.TimeField(default=datetime.now)
     def __str__(self):
         return 'from: ' + self.from_student.user.username + ' to: ' + self.to_tutor.user.username + ' course: ' + self.course + ' status: ' + ('pending' if self.status == 1 else 'accepted' if self.status == 2 else 'declined')
     
