5876af01480efe37522ea3e88539efe8f3eb77e6
Saarthak Gupta <79454214+saarthak2002@users.noreply.github.com>
2023-04-13 23:16:55 -0400

    admin changes

--- tutorme/admin.py
+++ tutorme/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import AppUser, Tutor, Request
+from .models import AppUser, Tutor, Request, Ratings, TutorTimes
 # Register your models here.
 
 admin.site.register(AppUser)
 admin.site.register(Tutor)
+admin.site.register(TutorTimes)
 admin.site.register(Request)
+admin.site.register(Ratings)
